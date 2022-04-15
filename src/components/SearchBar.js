64154c09a1fa959a36b94b0ae0c1576ff295ba9d
khazarglzd <101716414+khazarglzd@users.noreply.github.com>
2022-04-15 14:39:14 +0400

    creating components, adding states and props.

---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-row mb-5 mt-4">
+          <div className="col-12">
+            <input
+              onChange={this.props.searchMovieProp}
+              type="text"
+              className="form-control"
+              placeholder="Search a movie..."
+            />
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
